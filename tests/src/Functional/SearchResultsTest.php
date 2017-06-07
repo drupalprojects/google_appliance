997d5656170b88f167d538e81d3db324da45937b
Lee Rowlands <lee.rowlands@previousnext.com.au>
2017-06-07 13:08:45 +1000

    Issue #2702993 by larowlan: Drupal 8 Port - skeleton results test

---
+++ tests/src/Functional/SearchResultsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\Tests\google_appliance\Functional;
+
+/**
+ * Defines a class for
+ */
+class SearchResultsTest extends GoogleApplianceFunctionalTestBase {
+
+  protected static $modules = [
+    'google_appliance_test',
+  ];
+
+  /**
+   * Tests search results.
+   */
+  public function testSearchResults() {
+
+  }
+
+}
