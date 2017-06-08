9f77ae543283919d12a86742c690cdd8f5ac1614
Lee Rowlands <lee.rowlands@previousnext.com.au>
2017-06-08 15:52:28 +1000

    Issue #2702993 by larowlan: Drupal 8 Port - update status, cleanup dead code

--- google_appliance.api.php
+++ google_appliance.api.php
@@ -35,42 +35,15 @@ use Drupal\Core\Url;
  * list of search parameters is available in the Google developer documentation
  * referenced below.
  *
- * @param $query
- *   The search query, represented as multi-dimensional, associative array just
- *   before it is sent to the search appliance.
+ * @param $params
+ *   The search params before they're sent to the host.
  *
- * @see google_appliance_search_view()
+ * @see \Drupal\google_appliance\Service\Search::search
  * @see https://developers.google.com/search-appliance/documentation/614/xml_reference#request_parameters
  */
-function hook_google_appliance_query_alter(&$query) {
+function hook_google_appliance_query_alter(&$params) {
   // Only return English language results.
-  $query['gsa_query_params']['lr'] = 'en';
-
-  // Alter the appliance host.
-  $query['gsa_host'] = 'http://gsa.example.com';
-}
-
-/**
- * Alter cURL request sent to Google Search Appliance.
- *
- * This hook is invoked after Google Search Appliance builds the query, just
- * before the query is sent to the appliance.
- *
- * Use this to alter options of the cURL request.
- *
- * @param $options
- *   The cURL options, represented as multi-dimensional, associative array just
- *   before it is sent to the search appliance.
- *
- * @see google_appliance_search_view()
- * @see http://www.php.net/manual/en/function.curl-setopt.php
- */
-function hook_google_appliance_curl_alter(&$options) {
-  // Enable verbose logging to STDERR.
-  $options[CURLOPT_VERBOSE] = TRUE;
-
-  // Set HTTP proxy to tunnel requests through.
-  $options[CURLOPT_PROXY] = 'http://gsa-proxy.example.com';
+  $query['lr'] = 'en';
 }
 
 /**
@@ -82,31 +55,13 @@ function hook_google_appliance_curl_alter(&$options) {
  * Use this to make result alterations that are inappropriate at the theme
  * level.
  *
- * @param $results
- *   The search results, represented as a multi-dimensional associative array.
+ * @param \Drupal\google_appliance\SearchResults\ResultSet $results
+ *   The search results.
  *
- * @param $payload
- *   The XML returned by the search appliance as a SimpleXMLElement object.
- *   Although this is alterable, it's unlikely you will want to make changes to
- *   the XML payload.
- *
- * @see google_appliance_parse_device_response_xml()
+ * @see \Drupal\google_appliance\Service\Search::search
  */
-function hook_google_appliance_results_alter(&$results, &$payload) {
-  // Simple example: alter all titles returned in the result set.
-  foreach ($results['entry'] as &$result) {
-    $result['title'] = str_replace('foo', 'bar', $result['title']);
-  }
-  unset($result);
-
-  // Advanced example: parse and return onebox results from the payload.
-  foreach ($payload->xpath('//OBRES') as $onebox) {
-    // Code to parse each onebox here.
-    $ob_return = ['foo' => 'bar', 'baz' => []];
-
-    // Add the onebox to the results array.
-    $results['oneboxes'][] = $ob_return;
-  }
+function hook_google_appliance_response_alter(Drupal\google_appliance\SearchResults\ResultSet $results) {
+  $results->addSynonym(new \Drupal\google_appliance\SearchResults\Synonym('Foo', 'foo'));
 }
 
 /**
@@ -125,6 +80,8 @@ function hook_google_appliance_results_alter(&$results, &$payload) {
  *
  * @see google_appliance_get_clusters()
  * @see theme_item_list()
+ *
+ * @todo Update as appropriate.
  */
 function hook_google_appliance_cluster_list_alter(&$cluster_list, $cluster_results) {
   // Add some CSS classes.
